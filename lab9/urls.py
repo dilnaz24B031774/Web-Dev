83bce9989525e0dabe09cf0e3f64fb515b02867a
dilnaz24B031774 <dilnaaz0307@gmail.com>
2026-03-31 15:07:03 +0500

    Add files via upload

--- lab9/urls.py
+++ lab9/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, ProductViewSet
+
+router = DefaultRouter()
+router.register('categories', CategoryViewSet)
+router.register('products', ProductViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('', include(router.urls)),
 ]
